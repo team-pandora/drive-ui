67ee33ddbadbac792479bdac109a79a832b798c7
Guy <guybar135@gmail.com>
2022-06-20 18:08:44 +0300

    feat: add router.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,9 @@ import { Box, Stack } from '@mui/material';
 import i18next from 'i18next';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Redirect, Route, Switch } from 'react-router-dom';
 import Sidebar from './components/layout/sideNav/Sidebar';
 import Topbar from './components/layout/Topbar';
-import Favorites from './pages/Favorites';
-import IncomingCargo from './pages/IncomingCargo';
-import IncomingTomcal from './pages/IncomingTomcal';
-import MyDrive from './pages/MyDrive';
-import Quota from './pages/Quota';
-import Recently from './pages/Recently';
-import Shared from './pages/Shared';
-import StatusTransferred from './pages/StatusTransferred';
-import Trash from './pages/Trash';
+import { Router } from './router';
 
 function App() {
     useTranslation();
@@ -34,42 +25,7 @@ function App() {
             <Topbar />
             <Stack direction="row">
                 <Sidebar />
-                <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/my-drive" />
-                    </Route>
-                    <Route path="/my-drive" exact>
-                        <MyDrive />
-                    </Route>
-                    <Route path="/folder/:folderId" exact>
-                        <MyDrive />
-                    </Route>
-                    <Route path="/shared" exact>
-                        <Shared />
-                    </Route>
-                    <Route path="/recently" exact>
-                        <Recently />
-                    </Route>
-                    <Route path="/favorites" exact>
-                        <Favorites />
-                    </Route>
-                    <Route path="/trash" exact>
-                        <Trash />
-                    </Route>
-                    <Route path="/storage" exact>
-                        <Quota />
-                    </Route>
-                    {/* TODO: */}
-                    <Route path="/external-transferred-dropbox">
-                        <IncomingTomcal />
-                    </Route>
-                    <Route path="/external-transferred-cargo">
-                        <IncomingCargo />
-                    </Route>
-                    <Route path="/statusTransferred">
-                        <StatusTransferred />
-                    </Route>
-                </Switch>
+                <Router />
             </Stack>
         </Box>
     );
---
+++ src/router.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Favorites from './pages/Favorites';
+import IncomingCargo from './pages/IncomingCargo';
+import IncomingTomcal from './pages/IncomingTomcal';
+import MyDrive from './pages/MyDrive';
+import Quota from './pages/Quota';
+import Recently from './pages/Recently';
+import Shared from './pages/Shared';
+import StatusTransferred from './pages/StatusTransferred';
+import Trash from './pages/Trash';
+
+export const Router: React.FC = () => {
+    return (
+        <Switch>
+            <Route path="/" exact>
+                <Redirect to="/my-drive" />
+            </Route>
+            <Route path="/my-drive" exact>
+                <MyDrive />
+            </Route>
+            <Route path="/folder/:folderId" exact>
+                <MyDrive />
+            </Route>
+            <Route path="/shared" exact>
+                <Shared />
+            </Route>
+            <Route path="/recently" exact>
+                <Recently />
+            </Route>
+            <Route path="/favorites" exact>
+                <Favorites />
+            </Route>
+            <Route path="/trash" exact>
+                <Trash />
+            </Route>
+            <Route path="/storage" exact>
+                <Quota />
+            </Route>
+            {/* TODO: */}
+            <Route path="/external-transferred-dropbox">
+                <IncomingTomcal />
+            </Route>
+            <Route path="/external-transferred-cargo">
+                <IncomingCargo />
+            </Route>
+            <Route path="/statusTransferred">
+                <StatusTransferred />
+            </Route>
+        </Switch>
+    );
+};
