4215b522cb912487506ad562096cd6aac3ddbd42
Guy <guybar135@gmail.com>
2022-06-21 16:40:55 +0300

    refactor: changing styled instead of switching between styled components in navButton

--- src/components/layout/sideNav/NavButton.tsx
+++ src/components/layout/sideNav/NavButton.tsx
@@ -8,28 +8,26 @@ const NavLinkStyle = styled(NavLink)({
     borderRadius: '25px 0 0 25px',
 });
 
-const ActiveTypography = styled(Typography)({
-    fontWeight: 'bold',
-});
-
 type props = {
     path: string;
     label: string;
     children: React.ReactNode;
 };
 
-// TODO: pass icon as children instead of prop.
 const NavButton: React.FC<props> = ({ path, label, children }) => {
     const dir = i18next.dir(i18next.language) === 'rtl' ? '25px 0 0 25px' : '0 25px 25px 0';
-    const ActiveListItem = styled(ListItem)({
-        backgroundColor: '#e8f0fe',
-        borderRadius: i18next.dir(i18next.language) === 'rtl' ? '25px 0 0 25px' : '0 25px 25px 0',
-    });
 
     const location = useLocation();
     const isActive = location.pathname === `/${path}`;
-    const ListItemComponent = isActive ? ActiveListItem : ListItem;
-    const TypographyComponent = isActive ? ActiveTypography : Typography;
+
+    const SListItem = styled(ListItem)({
+        backgroundColor: isActive ? '#e8f0fe' : 'inherit',
+        borderRadius: i18next.dir(i18next.language) === 'rtl' ? '25px 0 0 25px' : '0 25px 25px 0',
+    });
+
+    const STypography = styled(Typography)({
+        fontWeight: isActive ? 'bold' : 'normal',
+    });
 
     const iconButtonStyle = {
         backgroundColor: `${children === null ? '#f4f2ed' : 'none'}`,
@@ -38,12 +36,12 @@ const NavButton: React.FC<props> = ({ path, label, children }) => {
 
     return (
         <NavLinkStyle activeStyle={{ color: '#1967d2' }} to={path}>
-            <ListItemComponent sx={{ marginTop: '5px' }} disablePadding>
+            <SListItem sx={{ marginTop: '5px' }} disablePadding>
                 <ListItemButton sx={iconButtonStyle}>
                     <ListItemIcon sx={{ minWidth: '36px', marginX: '10px' }}>{children}</ListItemIcon>
-                    <TypographyComponent>{label}</TypographyComponent>
+                    <STypography>{label}</STypography>
                 </ListItemButton>
-            </ListItemComponent>
+            </SListItem>
         </NavLinkStyle>
     );
 };
